06111641cb81df7f959f519931e232db21240486
Garik1981ua <Garik1981ua@gmail.com>
2019-04-09 20:24:17 +0300

    LESSON1

---
+++ src/com/blackClover/lesson1/SimpleApplication.java
@@ -0,0 +1,12 @@
+package com.blackClover.lesson1;
+
+public class SimpleApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        short s = 140;
+        byte b = (byte) s;
+        System.out.println(b);
+    }
+}
